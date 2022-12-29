8c1b6cf17aa5ba56e33628d51a672cf438bae7ff
鴨嘴獸(Lzt) <68383526+lzt0104@users.noreply.github.com>
2022-12-29 20:51:27 +0800

    Update 20221227p.py

--- 20221227p.py
+++ 20221227p.py
@@ -50,16 +50,46 @@ while True:
     st = input("輸入檢測字元(end結束)")
     if st!="end":
         sa = input("檢測的單一字元:")
-        
+        sb = input("取代字元與取代次數(字元 次數)").split(" ")
+        print("字元"+str(sa)+"出現次數:",list(sa).count(sa))
+        print("取代後字串:",st.replace(sa,sb[0],int(sb[1])))
     else:
         print("檢測結束")
         break
 
+right = ['red','blue','red','green']
+for i in range(10):
+    hint = []
+    user = input('依序輸入四個顏色(空白間隔)').split(' ')
+    for color in range(4):
+        if right.count(user[color])>0:
+            if right[color]==user[color]:
+                hint.insert(color,"1")
+            elif right[color]!=user[color]:
+                hint.insert(color,'2')
+        elif right.count(user[color])<=0:
+            hint.insert(color,'3')
+
+    if hint.count('1')==4:
+        print('正確答案')
+        break
+    elif hint.count('1')!=4:
+        print(hint[0]+hint[1]+hint[2]+hint[3])
+
+
+aa = []
+for i in range(10):
+    user = int(input('請輸入第'+str(i+1)+'個數字:'))
+    aa.append(user)
+aa.sort()
+print('最小3個數字:',aa[0],',',aa[1],',',aa[2])
+aa.reverse()
+print('最大3個數字:',aa[0],',',aa[1],',',aa[2])
 
 
 
 
 
 
-f = open("data.txt",'r',encoding='cp950')
+
 
